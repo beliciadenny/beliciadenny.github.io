eacec9f14093653ce09a2208607ddcb9a04dd944
Belicia Denny <168287965+beliciadenny@users.noreply.github.com>
2026-07-27 17:52:33 +0530

    Update script.js

--- data-analytics/script.js
+++ data-analytics/script.js
@@ -82,3 +82,36 @@ function revealSections(){
 window.addEventListener("scroll",revealSections);
 
 revealSections();
+// ==========================================
+// Back To Top
+// ==========================================
+
+const topBtn=document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>500){
+
+        topBtn.style.display="block";
+
+    }
+
+    else{
+
+        topBtn.style.display="none";
+
+    }
+
+});
+
+topBtn.addEventListener("click",()=>{
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
